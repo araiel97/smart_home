fe0a239cf51a067df1557fcbecf259046d05ed8c
Dominik Kot <domkot42@wp.pl>
2021-08-10 07:38:51 +0200

    implementacja REST API ciąg dalszy

---
+++ smart_home/src/main/java/com/example/smart_home/controller/TempSensorController.java
@@ -0,0 +1,39 @@
+package com.example.smart_home.controller;
+
+import com.example.smart_home.entity.TempSensor;
+import com.example.smart_home.service.TempSensorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TempSensorController {
+    @Autowired
+    private TempSensorService service;
+    public TempSensor temp_sensor;
+
+    @PostMapping("/addTempSensor")
+    public TempSensor addSensor(@RequestBody TempSensor temp_sensor){
+        return service.add_sensor(temp_sensor);
+    }
+    @PostMapping("/addTempSensors")
+    public List<TempSensor> addSensors(@RequestBody List<TempSensor> temp_sensors){
+        return service.add_sensors(temp_sensors);    }
+    @GetMapping("/TempSensors")
+    public List<TempSensor> findTempSensors(){
+        return service.findTempSensors();
+    }
+    @GetMapping("/TempSensor/{id}")
+    public TempSensor findTempSensorByID(@PathVariable int id){
+        return service.findTempSensorById(id);
+    }
+    @GetMapping("/TempSensor/{name}")
+    public TempSensor findTempSensorByName(@PathVariable String name){
+        return service.findTempSensorByName(name);
+    }
+    @PutMapping("/TempSensor/update")
+    public TempSensor updateTempSensor(@RequestBody TempSensor temp_sensor) {  return service.updateTempSensor(temp_sensor);  }
+    @DeleteMapping("/TempSensor/delete/{id}")
+    public String deleteTempSensor(@PathVariable int id) { return service.deleteTempSensor(id); }
+}
